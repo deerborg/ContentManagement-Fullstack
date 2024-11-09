2aea44db9115efe37b71c3bb9ffcdce9de9ed994
deerborg <furkanaydem1r@hotmail.com>
2024-11-09 16:07:24 +0300

    Improve application initialization, global exception handling, API response structure, and user validation
    - Refactor ContentManagementApiApplication.java to enhance application startup configuration and performance.
    - Extend GlobalHandleException.java to manage additional exception cases, improving global error handling consistency.
    - Update ApiResponseHelper.java to standardize API responses, ensuring clearer communication of success and error messages.
    - Refine UserValidation.java by adding new validation rules for user data integrity and security.
    - Add custom exception classes in the user module to handle specific user-related errors (untracked file in /user/model/util/exception/).

--- ContentManagement-BE/src/main/java/dev/deerops/contentmanagementapi/common/util/exception/GlobalHandleException.java
+++ ContentManagement-BE/src/main/java/dev/deerops/contentmanagementapi/common/util/exception/GlobalHandleException.java
@@ -5,6 +5,9 @@ import dev.deerops.contentmanagementapi.common.util.result.ApiResponseHelper;
 import dev.deerops.contentmanagementapi.content.model.util.exception.ContentLimitExceededException;
 import dev.deerops.contentmanagementapi.content.model.util.exception.NotFoundContent;
 import dev.deerops.contentmanagementapi.content.model.util.exception.NullOrEmptyContentException;
+import dev.deerops.contentmanagementapi.user.model.util.exception.EmptyOrNullFieldForUserException;
+import dev.deerops.contentmanagementapi.user.model.util.exception.InvalidMailFormatException;
+import dev.deerops.contentmanagementapi.user.model.util.exception.InvalidPhoneFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,4 +31,18 @@ public class GlobalHandleException {
         return new ResponseEntity<>(ApiResponseHelper.EXCEEDED_CONTENT(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(EmptyOrNullFieldForUserException.class)
+    public ResponseEntity<?> handleEmptyOrNullFieldForUserException() {
+        return new ResponseEntity<>(ApiResponseHelper.NULL_OR_EMPTY_USER_FIELD(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidMailFormatException.class)
+    public ResponseEntity<?> handleInvalidMailFormatException() {
+        return new ResponseEntity<>(ApiResponseHelper.INVALID_MAIL_FORMAT(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidPhoneFormatException.class)
+    public ResponseEntity<?> handleInvalidPhoneFormatException() {
+        return new ResponseEntity<>(ApiResponseHelper.INVALID_PHONE_FORMAT(), HttpStatus.BAD_REQUEST);
+    }
 }
